2635118484e0a732458e745a46cfe6c3b705c1d5
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2025-04-13 13:45:58 +0200

    Zadanie 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+// ----- Roznica miedzy klasa, a obiektem -----
+
+// Klasa:
+// Klasa jest kolokwialnie mowiac szablonem na podstawie ktorego tworzymy obiekty np: 
+// Klasa Auto która mówi nam o jego kolorze, marce itd.
+
+// Obiekt: 
+// Obiekt jest instancja klasy czyli jest to konkretne Auto ktore moge stworzyc na przykladzie klasy.
+
+class Car {
+    String brand;
+    String color;
+    int madeYear;
+
+    void info() {
+        System.out.println("Marka: " + brand);
+        System.out.println("Kolor: " + color);
+        System.out.println("Rok produkcji: " + madeYear);
+    }
+}
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Tworze nowy obiekt uzywajac klasy Car
+        Car myNewCar = new Car();
+        myNewCar.brand = "Toyota";
+        myNewCar.color = "Grey";
+        myNewCar.madeYear = 2020;
+
+        // Wywoluje funkcje info() z obiektu myNewCar
+        myNewCar.info();
     }
 }
